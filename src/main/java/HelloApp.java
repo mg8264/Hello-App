b3eb69d6094742ba35e3a67bd42e1a77a1f46ad0
mg8264 <mg8264@srmist.edu.in>
2026-04-12 20:02:02 +0530

    Feature-UC6

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,17 +2,14 @@ public class HelloApp {
     public static void main(String[] args) {
         
         StringBuilder nameBuilder = new StringBuilder();
-        boolean first = true;
         for(String name : args)
         {
-            if(!first)
-            {
-                nameBuilder.append(", ");
-            }
-            nameBuilder.append(name);
-            first = false;
+            nameBuilder.append(name).append(", ");
         }
         String name = "World";
+        if(nameBuilder.length() > 0){
+            name = nameBuilder.substring(0,nameBuilder.length() - 2);
+        }
         System.out.println("Hello, "+ name +"!");
     }
 }
